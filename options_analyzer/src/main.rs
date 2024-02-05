12e693d0150f086de9a3840c3dc386f4eeb13afa
kennardpeters <kennardpeters@umail.ucsb.edu>
2024-02-05 08:50:17 -0800

    Added timer to main in order to test different scraper implementations

--- options_analyzer/src/main.rs
+++ options_analyzer/src/main.rs
@@ -1,4 +1,9 @@
+use std::time::Instant;
+
 
 fn main() {
+    let now = Instant::now();
     println!("Hello, world!");
+    
+    println!("Running hello world!() took {} seconds.", now.elapsed().as_secs());
 }
