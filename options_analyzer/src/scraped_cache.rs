a29ef3ff05d020be77b27d784fc8154e281ba932
kennardpeters <kennardpeters@umail.ucsb.edu>
2024-04-03 19:28:16 -0700

    finished test_set for caches

--- options_analyzer/src/scraped_cache.rs
+++ options_analyzer/src/scraped_cache.rs
@@ -64,7 +64,8 @@ impl ScrapedCache {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::sync::Arc;
+    use std::{collections::HashMap, sync::Arc};
+    use serde_json::to_string;
     use tokio::sync::oneshot;
     #[tokio::test]
     async fn test_get() -> Result<(), Box<dyn std::error::Error>> {
@@ -235,14 +236,6 @@ mod tests {
         //Create multiple threads to get multiple keys
         let mut contract_cache = Arc::new(tokio::sync::Mutex::new(ScrapedCache::new(100)));
 
-        //Set mock contract in cache 
-        let mut contract = Contract::new();
-        let last_price = -1.0;
-        contract.contract_name = "test".to_string();
-        contract.last_price = last_price.clone();
-
-        //set the cache in the 
-        contract_cache.lock().await.set("test".to_string(), contract.clone()).await;
 
         //Caching thread
         tokio::spawn(async move {
@@ -268,44 +261,142 @@ mod tests {
    
         //Create multiple threads to get example key 
         let tx1 = tx.clone();
-        tokio::spawn(async move {
+        let contract_name1 = "test1";
+        let last_price1 = -1.0;
+        let thread1 = tokio::spawn(async move {
             let (resp_tx, resp_rx) = oneshot::channel();
-            let command = Command::Get{
-                key: "test".to_string(),
+            //Set mock contract in cache 
+            let mut contract = Contract::new();
+            contract.contract_name = contract_name1.clone().to_string();
+            contract.last_price = last_price1.clone();
+
+            let command = Command::Set{
+                key: contract_name1.clone().to_string(),
+                value: contract.clone(),
                 resp: resp_tx,
             };
             match tx1.send(command).await {
                 Ok(_) => {},
                 Err(e) => {
-                    let msg = format!("scraped_cache::test_get - Error occurred while requesting contract from cache: {}", e);
+                    let msg = format!("scraped_cache::test_set - Error occurred while sending contract to cache: {}", e);
                     println!("{}", msg);
                 },
             };
             let resp = match resp_rx.await {
                 Ok(x) => x,
                 Err(e) => {
-                    let msg = format!("scraped_cache::test_get - Error occurred while receiving contract from cache: {}", e);
+                    let msg = format!("scraped_cache::test_set - Error occurred while receiving result of sending contract from cache: {}", e);
                     println!("{}", msg);
                     Err(()) 
                 },
             };
-            let contract1 = match resp {
-                Ok(x) => {
-                    x.unwrap()
-                },
+
+        });
+
+        let tx2 = tx.clone();
+        let contract_name2 = "test2";
+        let last_price2 = -2.0;
+        let thread2 = tokio::spawn(async move {
+            let (resp_tx, resp_rx) = oneshot::channel();
+            //Set mock contract in cache 
+            let mut contract = Contract::new();
+            contract.contract_name = contract_name2.clone().to_string();
+            contract.last_price = last_price2.clone();
+
+            let command = Command::Set{
+                key: contract_name2.clone().to_string(),
+                value: contract.clone(),
+                resp: resp_tx,
+            };
+            match tx2.send(command).await {
+                Ok(_) => {},
                 Err(e) => {
-                    let msg = format!("scraped_cache::test_get - Error occurred while receiving contract from cache: {:?}", e);
+                    let msg = format!("scraped_cache::test_set - Error occurred while sending contract to cache: {}", e);
                     println!("{}", msg);
-                    Contract::new()
+                },
+            };
+            let resp = match resp_rx.await {
+                Ok(x) => x,
+                Err(e) => {
+                    let msg = format!("scraped_cache::test_set - Error occurred while receiving result of sending contract from cache: {}", e);
+                    println!("{}", msg);
+                    Err(()) 
                 },
             };
 
-            assert_eq!(contract1.last_price, last_price.clone());
-            Ok::<Contract, ()>(contract1)
         });
-        //Create multiple threads to set/get multiple keys
-        //TODO:
-        Err("Not implemented")?
+
+
+        let tx3 = tx.clone();
+        let contract_name3 = "test3";
+        let last_price3 = -3.0;
+        let thread3 = tokio::spawn(async move {
+            let (resp_tx, resp_rx) = oneshot::channel();
+            //Set mock contract in cache 
+            let mut contract = Contract::new();
+            contract.contract_name = contract_name3.clone().to_string();
+            contract.last_price = last_price3.clone();
+
+            let command = Command::Set{
+                key: contract_name3.clone().to_string(),
+                value: contract.clone(),
+                resp: resp_tx,
+            };
+            match tx3.send(command).await {
+                Ok(_) => {},
+                Err(e) => {
+                    let msg = format!("scraped_cache::test_set - Error occurred while sending contract to cache: {}", e);
+                    println!("{}", msg);
+                },
+            };
+            let resp = match resp_rx.await {
+                Ok(x) => x,
+                Err(e) => {
+                    let msg = format!("scraped_cache::test_set - Error occurred while receiving result of sending contract from cache: {}", e);
+                    println!("{}", msg);
+                    Err(()) 
+                },
+            };
+
+        });
+        //Match stmts here?
+        thread1.await;
+        thread2.await;
+        thread3.await;
+
+        let contracts = vec![contract_name1.clone(), contract_name2.clone(), contract_name3.clone()];
+        let mut expected = HashMap::new();
+        for i in contracts.iter() {
+            let (resp_tx, resp_rx) = oneshot::channel();
+            let command = Command::Get{
+                key: i.clone().to_string(),
+                resp: resp_tx,
+            };
+            match tx.send(command).await {
+                Ok(_) => {},
+                Err(e) => {
+                    let msg = format!("scraped_cache::test_set - Error occurred while requesting contract from cache: {}", e);
+                    println!("{}", msg);
+                },
+            };
+            let resp = match resp_rx.await {
+                Ok(x) => x,
+                Err(e) => {
+                    let msg = format!("scraped_cache::test_set - Error occurred while receiving result of sending contract from cache: {}", e);
+                    println!("{}", msg);
+                    Err(()) 
+                },
+            };
+            expected.insert(i.clone().to_string(), resp.unwrap().unwrap());
+        }
+
+
+
+        //Check the result of setting from multiple threads 
+        assert_eq!(last_price1, expected.get(contract_name1.clone()).unwrap().last_price);
+        assert_eq!(last_price2, expected.get(contract_name2.clone()).unwrap().last_price);
+        assert_eq!(last_price3, expected.get(contract_name3.clone()).unwrap().last_price);
+        Ok(())
     }
 
 }
